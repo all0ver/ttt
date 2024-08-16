5f424daadc17cb57bdc76968a5f1553dc18b4ea0
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-16 22:50:18 +0200

    added working restart button

--- docs/index.js
+++ docs/index.js
@@ -24,34 +24,53 @@ const gameboard = () => {
     board.fill(null);
   }
 
-  return { updateBoard, getBoard, resetBoard, board};
+  return { updateBoard, getBoard, resetBoard, board };
 }
+
+const info = document.querySelector(".info");
+const restart = document.querySelector(".restart");
 
 const game = (gameboard) => {
   let round = 0;
   let gameActive = true;
   const fields = document.querySelectorAll(".area");
 
-  fields.forEach((field,index) => {
+  fields.forEach((field, index) => {
     field.addEventListener("click", () => {
       if (gameActive === true && round < 9 && gameboard.board[index] == null) {
         if (round % 2 === 0) {
           gameboard.updateBoard(index, playerOne);
           field.textContent = "x";
+          info.textContent = '"O" Turn';
         } else {
           gameboard.updateBoard(index, playerTwo);
           field.textContent = "o";
+          info.textContent = '"X" Turn';
         }
         if (check(gameboard.board)) {
-          console.log("game over");
           gameActive = false;
-          endOfGame();
           return;
         }
         round++;
       }
     });
   });
+
+  const endOfGame = () => {
+    gameActive = false;
+    info.textContent = "Game Over";
+  }
+  restart.addEventListener("click", () => {
+    fields.forEach(field => {
+      field.textContent = "";
+    })
+    gameboard.resetBoard();
+    round = 0;
+    gameActive = true;
+    info.textContent = '"X" Turn';
+  })
+  return { endOfGame };
+  // TODO: read more about objects and returning them
 }
 
 function check(board) {
@@ -59,16 +78,15 @@ function check(board) {
   const winPosition = ['012', '345', '678', '036', '147', '258', '048', '642'];
 
   for (let win of winPosition) {
-    const [a, b, c] = [win[0],win[1],win[2]].map(Number);
+    const [a, b, c] = [win[0], win[1], win[2]].map(Number);
     if (board[a] !== null && board[a] === board[b] && board[a] === board[c]) {
-      console.log("winning marker: "+board[a]);
+      info.textContent = "Winner: " + board[a];
       return true;
     }
   }
   return false;
 }
 
-function endOfGame() {
-  console.log("zacznij nową gierkę");
-}
 let game1 = game(gameboard());
+
+
